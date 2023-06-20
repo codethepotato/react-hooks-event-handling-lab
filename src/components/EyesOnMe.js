a1b345140b71c01e832b963c38323953dc294b74
Erica Belzer <belzer49@gmail.com>
2023-06-20 13:28:10 -0500

    finished

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    return (
+        <div>
+            <button
+                onBlur={(e) => {
+                    console.log('Hey! Eyes on me!')}
+                    }
+                onFocus={(e) => {
+                    console.log('Good!')}} > 
+            </button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
